204b3ffd3bc8a6ec8645e472704956bd53d2910e
thomas444ortiz <thomasortiz444@gmail.com>
2024-01-04 12:07:02 -0500

    db up and running, everything connected

--- server/controllers/signupcontroller.js
+++ server/controllers/signupcontroller.js
@@ -1,27 +1,32 @@
 const models = require('../models/userModels');
-
+const assert = require('assert');
 //create an object that will store the middleware functions as methods, so we can export later
 const signupController = {};
 
 //function to get all users
 signupController.getAllUsers = (req, res, next) => {
-    res.locals.data = "placeholder for data, get all users worked"
-    next();
+    models.User.find()
+    .then((data)=> {
+        res.locals.data = data;
+        return next();
+    })
+    .catch(err => next(err));
 }
 
 //create a function to create a new user
 signupController.createUser = (req, res, next) => {
-    res.locals.data = 'hardcoded nodemon'
+    console.log(req.body);
 
-    // console.log(models.User.insertOne);
-
-    // models.User.insertOne({username: req.body.username, password: req.body.password})
-    // .then((data)=> {
-    //     console.log(data);
-    //     return next();
-    // })
-    // .catch(err => next(err));
-    next();
+    models.User.create({username: `${req.body.username}`, password: `${req.body.password}`})
+    .then((data) => {
+        console.log('inserted')
+        res.locals.data = data;
+        return next();
+    })
+    .catch(err => {
+        console.log(err);
+        return next(err)
+    })
 }
 
 //function to update user info
--- server/models/userModels.js
+++ server/models/userModels.js
@@ -8,14 +8,8 @@ mongoose.connect(MONGO_URI)
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
+    username: String,
+    password: String
 });
 
 const User = mongoose.model('User',userSchema);
